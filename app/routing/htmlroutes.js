79ff0f8503aaf07afa2c59c2a93d4a5c0e75d922
Katie DeAngelis <katie@Katies-MacBook-Pro.local>
2017-12-24 15:26:59 -0500

    range sliders

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -25,6 +25,10 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
+  app.get("css/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../assets/css/survey.css"));
+  });
+
   // If no matching route is found default to home
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
