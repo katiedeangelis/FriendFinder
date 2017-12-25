95f17fa6473a49983cab1542737590ec36d48f14
Katie DeAngelis <katie@Katies-MacBook-Pro.local>
2017-12-25 08:47:41 -0500

    Correct issue with stylesheet not rendering for survey,html

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -11,26 +11,26 @@ var path = require("path");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/home", function(req, res) {
+  app.get("/home", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
-  app.get("/survey", function(req, res) {
+  app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
-  app.get("css/survey", function(req, res) {
+  app.get("/survey.css", function (req, res) {
     res.sendFile(path.join(__dirname, "../assets/css/survey.css"));
   });
 
   // If no matching route is found default to home
-  app.get("*", function(req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
