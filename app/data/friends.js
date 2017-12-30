9e21941abf0cc9c1556e1257380a6fff7a775238
Katie DeAngelis <katie@Katies-MacBook-Pro.local>
2017-12-30 12:41:18 -0500

    Scraped slider due to issues with crossbrowser functionality.

--- app/data/friends.js
+++ app/data/friends.js
@@ -86,7 +86,7 @@ var friendArray = [{
     },
     {
         "name": "Jordan Biason",
-        "photo": "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+        "photo": "https://static.pexels.com/photos/428339/pexels-photo-428339.jpeg",
         "scores": [
             "4",
             "4",
